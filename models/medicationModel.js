840e67b3b431e844a702a1cf091f6c9623b50fbb
Alvin <2241801@slu.edu.ph>
2025-12-16 22:15:20 +0800

    added pharmacy column in medication table

--- models/medicationModel.js
+++ models/medicationModel.js
@@ -2,13 +2,18 @@ const pool = require('../config/db');
 
 const Medication = {
   async getAll() {
-    const [rows] = await pool.query('SELECT * FROM medication');
+    const [rows] = await pool.query(`
+      SELECT m.*, p.name AS pharmacyName
+      FROM medication m
+      LEFT JOIN pharmacy_medication pm ON m.medicationID = pm.medicationID
+      LEFT JOIN pharmacy p ON pm.pharmacyID = p.pharmacyID
+    `);
     return rows;
   },
 
   async getByPharmacyName(pharmacyName) {
     const [rows] = await pool.query(`
-      SELECT m.*
+      SELECT m.*, p.name AS pharmacyName
       FROM medication m
       JOIN pharmacy_medication pm ON m.medicationID = pm.medicationID
       JOIN pharmacy p ON pm.pharmacyID = p.pharmacyID
