22b85a263b3b69de700d71af631c1da015124bab
Keith Russel E. Hernandez <hernandezkeithrussel@gmail.com>
2025-12-17 11:54:58 +0800

    minor changes

--- controllers/medicationController.js
+++ controllers/medicationController.js
@@ -15,14 +15,6 @@ async function filterByPharmacyName(req, res, next) {
     const { pharmacyName } = req.query;
     const medications = await Medication.getByPharmacyName(pharmacyName);
 
-    if (!medications || medications.length === 0) {
-      return res.status(404).send(`
-        <h2>No Medications Found</h2>
-        <p>Pharmacy "${pharmacyName}" does not exist or has no medications assigned.</p>
-        <br><a href="/medications">Back to All Medications</a>
-      `);
-    }
-
     res.render('pages/medicationsByPharmacy', { medications, pharmacyName });
   } catch (err) {
     console.error('Medication Filter Error:', err);
